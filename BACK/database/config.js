3d9978844845ff9b34e1261e9a18cb47023b1228
Agustin Argañaraz <agustinarganarazz@gmail.com>
2025-09-19 14:28:36 -0300

    se crea configuracion con base de datos y se crea carpeta controllers

---
+++ BACK/database/config.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+const connection= mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'agustin1',
+        database:'dbfarmacia'
+    }
+)
+
+module.exports= {connection}
